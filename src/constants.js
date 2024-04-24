e9347cc88d7cf2c66cb18dc548986cb4c854018a
zhenyang <zhenyang455@gmail.com>
2024-04-25 02:16:58 +0800

    first commit

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I practice mostly in JavaScript and Java these days.
+      I've worked on a couple of projects mainly in Javascript using React and React Native. Anyway regardless of the language, I just enjoy programming.
+      Here is my <a href="https://github.com/Zehaych" target="_blank">Github</a>!`,
+
+    "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+    I like to watch programming tutorials on YouTube. The community for programming is really great such as <a href="https://www.youtube.com/@freecodecamp" target="_blank">freeCodeCamp</a> and <a href="https://www.youtube.com/@BroCodez" target="_blank">BroCode</a>! 
+    The way I built this portfolio was by learning from them!`,
+
+    tv: `That's my TV. This is one of my favorite places to relax and take a break whenever I feel stressed out from studying. It is also where I learn from some of the best teachers on the internet for example: <a href="https://www.youtube.com/@abdul_bari"> Abdul Bari</a>`,
+
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+
+    resume: `This is my desk and on it is my resume. <a href="https://github.com/Zehaych/resume/blob/main/Resume.pdf" target="_blank">Check it out?</a>
+    Contact me at zhenyang455@gmail.com if you have any interesting job opportunities!`,
+
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+
+    library: `There are a lot of programming books on my shelves. I've probably only ever actually read one of them.`,
+
+    exit: `If you want to exit Zhen Yang's portfolio, just close the tab.`,
+  };
